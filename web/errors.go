26c963d38aa0454a0779b22aedf623c861b963ab
mlogclub <mlog1@qq.com>
2023-03-22 22:56:56 +0800

    修改error用法

--- web/errors.go
+++ web/errors.go
@@ -8,19 +8,8 @@ func NewError(code int, text string) *CodeError {
 	return &CodeError{code, text, nil}
 }
 
-func NewErrorMsg(text string) *CodeError {
-	return &CodeError{0, text, nil}
-}
-
 func NewErrorData(code int, text string, data interface{}) *CodeError {
 	return &CodeError{code, text, data}
-}
-
-func FromError(err error) *CodeError {
-	if err == nil {
-		return nil
-	}
-	return &CodeError{0, err.Error(), nil}
 }
 
 type CodeError struct {

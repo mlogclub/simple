fa8d77e15d1a6dc27f993c5994dd218805ed2459
mlogclub <mlog1@qq.com>
2023-06-04 15:13:41 +0800

    新增一个方法

--- common/jsons/json.go
+++ common/jsons/json.go
@@ -2,11 +2,23 @@ package jsons
 
 import (
 	"encoding/json"
+
+	"github.com/mlogclub/simple/common/strs"
 	"github.com/sirupsen/logrus"
 )
 
 func Parse(str string, t interface{}) error {
+	if strs.IsAnyBlank(str) {
+		return nil
+	}
 	return json.Unmarshal([]byte(str), t)
+}
+
+func ParseBytes(bytes []byte, t interface{}) error {
+	if len(bytes) == 0 {
+		return nil
+	}
+	return json.Unmarshal(bytes, t)
 }
 
 func ToStr(t interface{}) (string, error) {

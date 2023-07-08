1ffa488e7068a0fce0bb6461b96d895d3d451188
mlogclub <mlog1@qq.com>
2023-07-08 20:13:57 +0800

    新的参数过滤方式

---
+++ web/params/query_filters.go
@@ -0,0 +1,74 @@
+package params
+
+import (
+	"strings"
+
+	"github.com/kataras/iris/v12"
+	"github.com/mlogclub/simple/common/strs"
+	"github.com/mlogclub/simple/common/strs/strcase"
+	"github.com/mlogclub/simple/sqls"
+)
+
+type QueryOp string
+
+const (
+	Eq       QueryOp = "eq"
+	Gt       QueryOp = "gt"
+	Lt       QueryOp = "lt"
+	Gte      QueryOp = "gte"
+	Lte      QueryOp = "lte"
+	Like     QueryOp = "like"
+	In       QueryOp = "in"
+	Starting QueryOp = "starting"
+	Ending   QueryOp = "ending"
+)
+
+type QueryFilter struct {
+	ParamName  string  // 请求参数名
+	Op         QueryOp // 操作符
+	ColumnName string  // 列名
+}
+
+func NewSqlCnd(ctx iris.Context, filters ...QueryFilter) *sqls.Cnd {
+	cnd := NewSqlCndWithoutPage(ctx, filters...)
+	p := GetPaging(ctx)
+	cnd.Page(p.Page, p.Limit)
+	return cnd
+}
+
+func NewSqlCndWithoutPage(ctx iris.Context, filters ...QueryFilter) *sqls.Cnd {
+	cnd := sqls.NewCnd()
+	for _, filter := range filters {
+		var (
+			columnName = filter.ColumnName
+			paramValue = ctx.FormValue(filter.ParamName)
+		)
+		if strs.IsBlank(columnName) {
+			columnName = strcase.ToSnake(filter.ParamName)
+		}
+		if strs.IsBlank(paramValue) {
+			continue
+		}
+		if filter.Op == Eq {
+			cnd.Eq(columnName, paramValue)
+		} else if filter.Op == Gt {
+			cnd.Gt(columnName, paramValue)
+		} else if filter.Op == Lt {
+			cnd.Lt(columnName, paramValue)
+		} else if filter.Op == Gte {
+			cnd.Gte(columnName, paramValue)
+		} else if filter.Op == Lte {
+			cnd.Lte(columnName, paramValue)
+		} else if filter.Op == Like {
+			cnd.Like(columnName, paramValue)
+		} else if filter.Op == Starting {
+			cnd.Starting(columnName, paramValue)
+		} else if filter.Op == Ending {
+			cnd.Ending(columnName, paramValue)
+		} else if filter.Op == In {
+			ss := strings.Split(paramValue, ",")
+			cnd.In(columnName, ss)
+		}
+	}
+	return cnd
+}

5b105541b814cf43425129538b2fcbdd593a3255
mlogclub <mlog1@qq.com>
2026-02-08 15:11:03 +0800

    add utility functions to retrieve client IP and User-Agent from HTTP requests

---
+++ web/utils.go
@@ -0,0 +1,31 @@
+package web
+
+import (
+	"net"
+	"net/http"
+	"strings"
+)
+
+// GetRequestIP 尽最大努力实现获取客户端 IP 的算法。
+// 解析 X-Real-IP 和 X-Forwarded-For 以便于反向代理（nginx 或 haproxy）可以正常工作。
+func GetRequestIP(r *http.Request) string {
+	xForwardedFor := r.Header.Get("X-Forwarded-For")
+	ip := strings.TrimSpace(strings.Split(xForwardedFor, ",")[0])
+	if ip != "" {
+		return ip
+	}
+
+	ip = strings.TrimSpace(r.Header.Get("X-Real-Ip"))
+	if ip != "" {
+		return ip
+	}
+
+	if ip, _, err := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr)); err == nil {
+		return ip
+	}
+	return ""
+}
+
+func GetUserAgent(r *http.Request) string {
+	return r.Header.Get("User-Agent")
+}
